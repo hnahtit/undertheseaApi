2e1fde2fb502dd438448f366741e27e80dd67e0f
hnahtit <ngtrungthanhcntt2@gmail.com>
2020-09-16 09:54:19 +0700

    add model pkl

---
+++ test.py
@@ -0,0 +1,21 @@
+import pickle
+from sklearn.externals import joblib
+#
+# Create your model here (same as above)
+#
+
+# Save to file in the current working directory
+pkl_filename = "sklearn_pipeline.pkl"
+# with open(pkl_filename, 'wb') as file:
+#     pickle.dump(model, file)
+
+# Load from file
+with open(pkl_filename, 'rb') as file:
+    pickle_model = joblib.load(file)
+
+newdata = [":(((","Hàng kém chất lượng"]
+# Calculate the accuracy score and predict target values
+# score = pickle_model.score(Xtest, Ytest)
+# print("Test score: {0:.2f} %".format(100 * score))
+Ypredict = pickle_model.predict(newdata)
+print(Ypredict)
